b43a974c639aaf4966cc7a5f708aae4a121eb9f0
Abel <Abel@Z10405N2.elumax.com>
2017-04-26 17:24:48 +0800

    EX 2017.04.26

--- src/ex/MainFrame.java
+++ src/ex/MainFrame.java
@@ -6,6 +6,7 @@
 package ex;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -13,6 +14,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javafx.scene.control.SelectionMode;
+import javax.swing.DefaultListSelectionModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,9 +25,11 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JToolBar;
 import javax.swing.JTree;
+import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.UIManager;
 import javax.swing.event.TreeExpansionEvent;
@@ -34,6 +39,7 @@ import javax.swing.event.TreeModelListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.event.TreeWillExpandListener;
+import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.DefaultTreeSelectionModel;
@@ -64,7 +70,6 @@ public class MainFrame extends JFrame {
     private JButton btnOpen;
     private JButton btnSave;
     private JMenuItem miClose;
-    private JTextArea taContent;
     private DefaultMutableTreeNode dmtnOOP;
     private DefaultMutableTreeNode dmtnC;
     private DefaultMutableTreeNode dmtnJava;
@@ -75,6 +80,7 @@ public class MainFrame extends JFrame {
     private TreeSelectionModel sm;
     private Dimension dimScreen;
     private JSplitPane sppTreeMain;
+    private JTable tbHR;
 
     public MainFrame() {
         initComponents();
@@ -96,7 +102,7 @@ public class MainFrame extends JFrame {
         initTreeScrollPane();
         initStatusBar();
         initSearch();
-        initTextArea();
+        initTableHR();
         initFrame();
     }
 
@@ -142,15 +148,18 @@ public class MainFrame extends JFrame {
     }
 
     private void initFrame() {
+        // Initial Frame
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Initial Menu Bar
         setJMenuBar(mbMain);
+        // Content Pane Layout
         getContentPane().add(tbMain, BorderLayout.NORTH);
         getContentPane().add(scpTreeMain, BorderLayout.WEST);
         getContentPane().add(plStatusBar, BorderLayout.SOUTH);
         getContentPane().add(plSearch, BorderLayout.EAST);
-        getContentPane().add(taContent, BorderLayout.CENTER);
+        getContentPane().add(tbHR, BorderLayout.CENTER);
         pack();
     }
 
@@ -284,7 +293,7 @@ public class MainFrame extends JFrame {
 //        scpTreeMain.setPreferredSize(new Dimension(200, 800));
         scpTreeMain.setWheelScrollingEnabled(true);
         sppTreeMain = new JSplitPane();
-        
+
     }
 
     private void initStatusBar() {
@@ -299,8 +308,11 @@ public class MainFrame extends JFrame {
         plSearch.add(keyword);
     }
 
-    private void initTextArea() {
-        taContent = new JTextArea();
+    private void initTableHR() {
+        tbHR = new JTable(60, 8);
+        tbHR.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        tbHR.setSelectionBackground(Color.YELLOW);
+        tbHR.setCellSelectionEnabled(true);
     }
 
     private void trMainValueChanged(TreeSelectionEvent e) {
